0aeb9d08d95e209f83fcbe05245182deefa4124e
Lilian Wu <ruofeiwu2022@u.northwestern.edu>
2022-05-03 15:29:22 -0500

    Add RDS credentials and data file path

--- config/flaskconfig.py
+++ config/flaskconfig.py
@@ -8,6 +8,22 @@ HOST = "0.0.0.0"
 SQLALCHEMY_ECHO = False  # If true, SQL for queries made will be printed
 MAX_ROWS_SHOW = 100
 
+# RDS credentials
+conn_type = "mysql+pymysql"
+host = os.environ.get("MYSQL_HOST")
+user = os.environ.get("MYSQL_USER")
+password = os.environ.get("MYSQL_PASSWORD")
+port = os.environ.get("MYSQL_PORT")
+db_name = os.environ.get("DATABASE_NAME")
+
 SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
 if SQLALCHEMY_DATABASE_URI is None:
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///data/tracks.db'
+    if host is None:
+        SQLALCHEMY_DATABASE_URI = 'sqlite:///data/tracks.db'
+    else:
+        SQLALCHEMY_DATABASE_URI = "{conn_type}://{user}:{password}@{host}:{port}/{db_name}"
+
+# file paths
+DATA_PATH = os.environ.get('DATA_PATH')
+if DATA_PATH is None:
+    DATA_PATH = 'raw/telecom_churn_data.csv'
