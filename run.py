2b4d5d0fd02bbf5bf599693e76e7a3b9b3e3d579
Lilian Wu <ruofeiwu2022@u.northwestern.edu>
2022-05-25 17:28:07 -0500

    Add code for implementing model pipeline

--- run.py
+++ run.py
@@ -3,8 +3,12 @@
 import argparse
 import logging.config
 
+import yaml
+
 from src.s3 import download_file_from_s3, upload_file_to_s3
+from src.data_handling import process_data
 from src.create_db import ChurnManager, create_db
+from src.modeling import train_model, make_predictions, eval_performance
 from config.flaskconfig import SQLALCHEMY_DATABASE_URI
 
 logging.config.fileConfig('config/logging/local.conf')
@@ -15,6 +19,9 @@ if __name__ == '__main__':
     # Add parsers for both creating a database and adding songs to it
     parser = argparse.ArgumentParser(
         description="Create and/or add data to database")
+    parser.add_argument('--config', default='config/config.yaml',
+                        help='Path to configuration file')
+
     subparsers = parser.add_subparsers(dest='command')
 
     sp_download = subparsers.add_parser("download_data", description="Download data from s3")
@@ -28,6 +35,8 @@ if __name__ == '__main__':
                            help="s3 data path to upload data")
     sp_upload.add_argument('--local_path', default='data/raw/raw_data.csv',
                            help="local data path to upload data")
+    # subparser for processing data
+    sp_process = subparsers.add_parser("process_raw", description="Process raw data")
 
     # subparser for creating a database
     sp_create = subparsers.add_parser("create_db", description="Create database")
@@ -36,24 +45,50 @@ if __name__ == '__main__':
 
     # subparser for ingesting data from cvs to the table in the database
     sp_ingest = subparsers.add_parser("ingest_data", description="Add data to database")
-    sp_ingest.add_argument("--input_path", default="data/raw/raw_data.csv",
+    sp_ingest.add_argument("--input_path", default="data/final/final_data.csv",
                            help="File path for data to be loaded into the database")
     sp_ingest.add_argument("--engine_string", default=SQLALCHEMY_DATABASE_URI,
                            help="SQLAlchemy connection URI for database")
 
+    # subparser for training the model
+    sp_train = subparsers.add_parser("train_model",
+                                     description="Train model and save trained model object")
+    # subparser for making predictions on the test set
+    sp_predict = subparsers.add_parser("predict",
+                                       description="Produce predictions from saved model")
+    # subparser for evaluating the model performance
+    sp_evaluate = subparsers.add_parser("evaluate",
+                                        description="Evaluate the model performance and save the result")
+
     args = parser.parse_args()
     sp_used = args.command
+
+    with open(args.config, "r") as f:
+        config = yaml.load(f, Loader=yaml.FullLoader)
 
     if sp_used == 'download_data':
         download_file_from_s3(args.local_path, args.s3_path)
     elif sp_used == 'upload_data':
         upload_file_to_s3(args.local_path, args.s3_path)
+    elif sp_used == 'process_raw':
+        process_data(**config['data_handling']['process_data'])
+        logger.info("Final data saved.")
     elif sp_used == 'create_db':
         create_db(args.engine_string)
-
     elif sp_used == 'ingest_data':
         cm = ChurnManager(engine_string=args.engine_string)
         cm.add_customer_data(args.input_path)
         cm.close()
+
+    elif sp_used == 'train_model':
+        train_model(**config['modeling']['train_model'])
+        logger.info("Random forest model saved.")
+    elif sp_used == 'predict':
+        make_predictions(**config['modeling']['make_predictions'])
+        logger.info("Prediction results saved.")
+    elif sp_used == 'evaluate':
+        eval_performance(**config['modeling']['eval_performance'])
+        logger.info("Model performance metrics saved.")
+
     else:
         parser.print_help()
