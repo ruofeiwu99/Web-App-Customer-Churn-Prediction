6a6a24717f473297b7d863ab16aa6abadf4dd2a5
Lilian Wu <ruofeiwu2022@u.northwestern.edu>
2022-05-27 15:07:35 -0500

    Change PORT, APP_NAME, nad MAX_ROWS_SHOW

--- config/flaskconfig.py
+++ config/flaskconfig.py
@@ -1,12 +1,12 @@
 import os
 DEBUG = True
 LOGGING_CONFIG = "config/logging/local.conf"
-PORT = 5000
-APP_NAME = "penny-lane"
+PORT = 5001
+APP_NAME = "churn-prediction"
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 HOST = "0.0.0.0"
 SQLALCHEMY_ECHO = False  # If true, SQL for queries made will be printed
-MAX_ROWS_SHOW = 100
+MAX_ROWS_SHOW = 10
 
 # RDS credentials
 conn_type = "mysql+pymysql"
@@ -17,6 +17,8 @@ port = os.environ.get("MYSQL_PORT")
 db_name = os.environ.get("DATABASE_NAME")
 
 SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+#host = None
+#SQLALCHEMY_DATABASE_URI = None
 
 if SQLALCHEMY_DATABASE_URI is not None:
     pass
