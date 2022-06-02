24da85dfc5f443504aa00f090d9c99105bb90daf
Lilian Wu <ruofeiwu2022@u.northwestern.edu>
2022-06-02 16:29:53 -0500

    Add unit test functions

---
+++ test/test_functions.py
@@ -0,0 +1,76 @@
+import pytest
+import pandas as pd
+
+from src.process_data import clean_data
+from src.modeling import select_target, select_features
+
+
+def test_select_target_valid_input():
+    """
+    Test select_target function with valid input
+    """
+    # load file for testing
+    data = pd.read_csv("test/unit_test_data/final_data_test.csv")
+    output_true = data['churn']
+    output_test = select_target(data, 'churn')
+    assert output_test.equals(output_true)
+
+
+def test_select_target_invalid_input():
+    """
+    Test select_target function with invalid input
+    """
+    # load file for testing
+    data = pd.read_csv("test/unit_test_data/final_data_test.csv")
+    with pytest.raises(KeyError):
+        select_target(data, 'invalid_churn')
+
+
+def test_select_features_valid_input():
+    """
+    Test select_features function with valid input
+    """
+    # load file for testing
+    data = pd.read_csv("test/unit_test_data/final_data_test.csv")
+    output_true = data[['churn', 'account_length']]
+    output_test = select_features(data, ['churn', 'account_length'])
+    # Test that the true and test are the same
+    pd.testing.assert_frame_equal(output_true, output_test)
+
+
+def test_select_features_empty_input():
+    """
+    Test select_features function with empty input
+    """
+    # load file for testing
+    data = pd.read_csv("test/unit_test_data/final_data_test.csv")
+    output_true = pd.DataFrame()
+    output_test = select_features(data, [])
+    # Test that the true and test are the same
+    assert output_test.equals(output_true)
+
+
+def test_select_features_invalid_input():
+    """
+    Test select_features function with invalid input
+    """
+    # load file for testing
+    data = pd.read_csv("test/unit_test_data/final_data_test.csv")
+    output_true = pd.DataFrame()
+    output_test = select_features(data, ['invalid_churn', 'invalid_account_length'])
+    # Test that the true and test are the same
+    assert output_test.equals(output_true)
+
+
+def test_clean_data_valid_input():
+    """
+    Test clean_data function with valid input
+    """
+    # load file for testing
+    data = pd.read_csv("test/unit_test_data/final_data_test.csv")
+    output_true = data[['account_length', 'custserv_calls', 'total_charges', 'churn']]
+    output_test = clean_data(data)
+    # Test that the true and test are the same
+    pd.testing.assert_frame_equal(output_true, output_test)
+
+
