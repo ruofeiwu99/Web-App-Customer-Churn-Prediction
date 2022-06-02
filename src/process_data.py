746c7f872df9376f7662c64c9cdc5f902ccfd29d
Lilian Wu <ruofeiwu2022@u.northwestern.edu>
2022-06-02 18:35:32 -0500

    Add exception handling and fix hard-corded column name

--- src/process_data.py
+++ src/process_data.py
@@ -8,14 +8,19 @@ logging.config.fileConfig('config/logging/local.conf')
 logger = logging.getLogger('process-data')
 
 
-def clean_data(data: pd.DataFrame) -> pd.DataFrame:
+def clean_data(data: pd.DataFrame, target: str) -> pd.DataFrame:
     """
     Clean raw dataframe
     Args:
         data (obj: pd.DataFrame): raw dataframe
+        target (str): target column name
     Returns:
         data (obj: pd.DataFrame): cleaned dataframe
     """
     # change churn labels to Yes/No
-    data['churn'] = data['churn'].map({0: 'No', 1: 'Yes'})
+    try:
+        data[target] = data[target].map({0: 'No', 1: 'Yes'})
+    except KeyError:
+        logger.error("Error: target column not found in the dataset. Please check the target column name.")
+        raise KeyError("%s is not in the input dataframe." % target)
     return data
