e888b2c0675982fa29a5ffd603807286fadc086e
Lilian Wu <ruofeiwu2022@u.northwestern.edu>
2022-06-02 20:37:02 -0500

    Add unit tests for process_data and modeling functions

--- test/test_functions.py
+++ test/test_functions.py
@@ -4,6 +4,8 @@ import pandas as pd
 from src.process_data import clean_data
 from src.modeling import select_target, select_features
 
+# pylint: disable=locally-disabled, invalid-name
+
 
 def test_select_target_valid_input():
     """
@@ -67,10 +69,18 @@ def test_clean_data_valid_input():
     Test clean_data function with valid input
     """
     # load file for testing
-    data = pd.read_csv("test/unit_test_data/final_data_test.csv")
-    output_true = data[['account_length', 'custserv_calls', 'total_charges', 'churn']]
-    output_test = clean_data(data)
+    raw_data = pd.read_csv("test/unit_test_data/raw_data_test.csv")
+    output_true = pd.read_csv("test/unit_test_data/final_data_test.csv")
+    output_test = clean_data(raw_data, 'churn')
     # Test that the true and test are the same
-    pd.testing.assert_frame_equal(output_true, output_test)
+    assert output_test.equals(output_true)
 
 
+def test_clean_data_invalid_input():
+    """
+    Test clean_data function with invalid column name
+    """
+    # load file for testing
+    raw_data = pd.read_csv("test/unit_test_data/raw_data_test.csv")
+    with pytest.raises(KeyError):
+        clean_data(raw_data, 'invalid_churn')
