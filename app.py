c98da108950900d531887c52809277071e1f212b
Lilian Wu <ruofeiwu2022@u.northwestern.edu>
2022-06-06 16:32:20 -0500

    Fix pylint issues

--- app.py
+++ app.py
@@ -9,30 +9,32 @@ import yaml
 import sqlalchemy.exc
 from flask import Flask, render_template, request, redirect, url_for
 
+# pylint: disable=locally-disabled, invalid-name
+
 # For setting up the Flask-SQLAlchemy database session
 from src.create_db import ChurnManager, Customer
 from src.modeling import pred_one_record
 from src.helper import validate_input
 
 # Initialize the Flask application
-app = Flask(__name__, template_folder="app/templates",
-            static_folder="app/static")
+app = Flask(__name__, template_folder='app/templates',
+            static_folder='app/static')
 
 # Configure flask app from flask_config.py
 app.config.from_pyfile('config/flaskconfig.py')
 
 # Define LOGGING_CONFIG in flask_config.py - path to config file for setting
 # up the logger (e.g. config/logging/local.conf)
-logging.config.fileConfig(app.config["LOGGING_CONFIG"])
-logger = logging.getLogger(app.config["APP_NAME"])
+logging.config.fileConfig(app.config['LOGGING_CONFIG'])
+logger = logging.getLogger(app.config['APP_NAME'])
 logger.debug(
     'Web app should be viewable at %s:%s if docker run command maps local '
     'port to the same port as configured for the Docker container '
     'in config/flaskconfig.py (e.g. `-p 5000:5000`). Otherwise, go to the '
     'port defined on the left side of the port mapping '
     '(`i.e. -p THISPORT:5000`). If you are running from a Windows machine, '
-    'go to 127.0.0.1 instead of 0.0.0.0.', app.config["HOST"]
-    , app.config["PORT"])
+    'go to 127.0.0.1 instead of 0.0.0.0.', app.config['HOST']
+    , app.config['PORT'])
 
 # Initialize the database session
 churn_manager = ChurnManager(app)
@@ -50,30 +52,30 @@ def index():
 
     """
     try:
-        customers = churn_manager.session.query(Customer).order_by(Customer.id.desc()).\
-            limit(app.config["MAX_ROWS_SHOW"])
+        customers = churn_manager.session.query(Customer).order_by(Customer.id).\
+            limit(app.config['MAX_ROWS_SHOW'])
 
-        logger.debug("Index page accessed")
+        logger.debug('Index page accessed')
         return render_template('index.html', customers=customers)
     except sqlite3.OperationalError as e:
         logger.error(
-            "Error page returned. Not able to query local sqlite database: %s."
-            " Error: %s ",
+            'Error page returned. Not able to query local sqlite database: %s.'
+            ' Error: %s ',
             app.config['SQLALCHEMY_DATABASE_URI'], e)
         return render_template('error.html')
     except sqlalchemy.exc.OperationalError as e:
         logger.error(
-            "Error page returned. Not able to query MySQL database: %s. "
-            "Error: %s ",
+            'Error page returned. Not able to query MySQL database: %s. '
+            'Error: %s ',
             app.config['SQLALCHEMY_DATABASE_URI'], e)
         return render_template('error.html')
     except:
         traceback.print_exc()
-        logger.error("Not able to display customer data, error page returned")
+        logger.error('Not able to display customer data, error page returned')
         return render_template('error.html')
 
 
-@app.route("/predict", methods=["POST"])
+@app.route('/predict', methods=['POST'])
 def predict_churn():
     """
         Predict customer churn given a POST form of input data.
@@ -110,18 +112,18 @@ def predict_churn():
     try:
         valid_record_df = validate_input(record_df)
     except ValueError as e:
-        logger.error("Error: %s", e)
+        logger.error('Error: %s', e)
         return render_template('error.html')
 
     # Predict churn label
-    with open('config/config.yaml', "r") as f:
+    with open('config/config.yaml', 'r') as f:
         config = yaml.load(f, Loader=yaml.FullLoader)
 
     try:
         with open('models/rf_model.pkl', 'rb') as f:
             rf_model = pickle.load(f)
     except FileNotFoundError:
-        logger.error("File not found, please check if model object is saved")
+        logger.error('File not found, please check if model object is saved')
     churn_pred = pred_one_record(rf_model,
                                  columns=config['modeling']['pred_one_record']['columns'],
                                  record_df=valid_record_df)
@@ -140,29 +142,29 @@ def predict_churn():
                                      valid_record_df['total_intl_calls'].item(),
                                      valid_record_df['customer_service_calls'].item(),
                                      final_churn_pred)
-        logger.info("One customer record added: customer id %s", request.form['id'])
+        logger.info('One customer record added: customer id %s', request.form['id'])
         return redirect(url_for('index'))
     except sqlite3.OperationalError as e:
         logger.error(
-            "Error page returned. Not able to add customer record to local sqlite "
-            "database: %s. Error: %s ",
+            'Error page returned. Not able to add customer record to local sqlite '
+            'database: %s. Error: %s ',
             app.config['SQLALCHEMY_DATABASE_URI'], e)
         return render_template('error.html')
     except sqlalchemy.exc.OperationalError as e:
         logger.error(
-            "Error page returned. Not able to add customer record to MySQL database: %s. "
-            "Error: %s ",
+            'Error page returned. Not able to add customer record to MySQL database: %s. '
+            'Error: %s ',
             app.config['SQLALCHEMY_DATABASE_URI'], e)
         return render_template('error.html')
     # exception handling for non-unique customer id
     except sqlalchemy.exc.IntegrityError as e:
         logger.error(
-            "Error page returned. Not able to add customer record to MySQL database: %s. "
-            "Error: %s ",
+            'Error page returned. Not able to add customer record to MySQL database: %s. '
+            'Error: %s ',
             app.config['SQLALCHEMY_DATABASE_URI'], e)
         return render_template('error.html')
 
 
 if __name__ == '__main__':
-    app.run(debug=app.config["DEBUG"], port=app.config["PORT"],
-            host=app.config["HOST"])
+    app.run(debug=app.config['DEBUG'], port=app.config['PORT'],
+            host=app.config['HOST'])
